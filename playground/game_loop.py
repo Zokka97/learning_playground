47de827a351fae331bad338e66bc52c4cce077c7
Zokka97 <joshuajao20010907@gmail.com>
2023-05-09 03:34:59 -0400

    update

--- playground/game_loop.py
+++ playground/game_loop.py
@@ -15,9 +15,10 @@ condition = {
     "start": input(),
     "critical_hit": {0, random.randrange(20, 25)},
     "buhamut_attack": {0, random.randrange(20, 50)},
+    "buhanut_soaring_fire": 100,
     "buhamut_prev_attack": state["player_health"]
 }
-Print(f"""
+print(f"""
     Welcome to the game
     Press any key to start
     {condition["start"]}
@@ -37,10 +38,20 @@ while condition["game_status"]:
         Press C to use Copy Enemy Attack
     """)
     if condition["player_input"] == "X":
-        state["buhamut_health"] -= random.randrange(1, 21)
-        - condition["critical_hit"]
+        state["buhamut_health"] -= (random.randrange(1, 21)
+                                    + condition["critical_hit"])
     elif condition["player_input"] == "Z":
         state["buhamut_health"] -= 50
-    elif condition["player_input"] == "C":
+    # elif condition["player_input"] == "C":
+
+    # elif condition["player_input"] == "C":
+    #     condition["buhamut_attack"]
+    if state["buhamut_health"] is (- 30 <= - 40):
+        condition["buhanut_soaring_fire"]
+    else:
         condition["buhamut_attack"]
-    elif condition["player_input"] == "C":
+"""
+NOTE: Store Buhamut attack damage into a variable
+      after the condition loop back
+
+ """
