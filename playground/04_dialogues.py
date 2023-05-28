81098bd9393a6d80b07f165b917acfdd1f503933
Zokka97 <joshuajao20010907@gmail.com>
2023-05-28 06:05:45 -0400

    update

--- playground/04_dialogues.py
+++ playground/04_dialogues.py
@@ -1,6 +1,8 @@
 # don't use the while loop only define function or other function
 # bad idea
-# while loop is important idk, why i did it but already started it. no turning back
+# while loop is important, idk why i did it but already started it. no turning back
+
+# this is the dictionary area
 v_names = {
     "elder": "Village Elder:",
 
@@ -10,6 +12,19 @@ player_status = {
     "bullets": 0
 
 }
+store_item = {
+    "bullets": 5,
+    "fish_crackers": 500
+}
+
+enemies = {
+    "flying_coconuts": 100,
+    "crawling_snakes": 200,
+    "four_legged_bird": 150,
+    "humongous_centipede": 200,
+    "howling_corns": 500
+}
+# this is the conversation area
 
 
 def convo_001():
@@ -48,11 +63,16 @@ def convo_003():
     x = input(">>> ").lower()
     if not x or x:
         print(
-            f"{v_names['elder']}By the way, what is your name young traveller?")
+            f"{v_names['elder']} By the way, what is your name young traveller?")
 
 
 def convo_004(player_name=None):
     player_name = input("Type your name: ").title()
+    if not player_name:
+        print("Okay I'll name you Ingeborg")
+        player_name = "Ingeborg"
+    else:
+        pass
     return player_name
 
 
@@ -71,41 +91,72 @@ def convo_006():
     player_status["bullets"] = 5
 
 
-def shop(bullets=10, fish_crakers=5):
-    """Buy Items from the shop"""
+def convo_007():
     x = input(">>> ").lower()
     if not x or x:
-        print(f"Buy 1 bullet? {bullets}")
-        print(f"Buy 1 fish crakers? {fish_crakers}")
-    buy = input("Press X to buy bullets \nPress Z to buy fish crakers: ").lower()
-    if buy == "x":
-        bullets -= 1
-        player_status["bullets"] += 1
-        return str(bullets) + " left from the shop"
-    elif buy == "z":
-        fish_crakers -= 1
-        player_status["health"] += 1
-        return str(fish_crakers) + " left from the shop"
+        print(
+            f"{v_names['elder']} You'll be able to enter the store after each battle")
+        print(
+            f"{v_names['elder']} You must kill all the monster to be able to exit the village")
+        print(f"{v_names['elder']} Goodluck with your mission!")
+
+# this is the enemy area
 
 
-# def health_shop():
-# def enemy_001():
-# def enemy_002():
-# def enemy_003():
-# def enemy_004():
-# def enemy_005():
+def enemy_001(flying_coconuts=100):
+
+    # this is the store area
 
 
-convo_001()
+def bullets():
+    bullets_before = store_item["bullets"]
+    store_item["bullets"] -= 1
+    player_status["bullets"] += 1
+    print(f"""
+There is {store_item['bullets']} out of {bullets_before} bullets
+You've have {player_status['bullets']} bullets
+    """)
 
-convo_002()
 
-restult_004 = convo_004()
-# print(restult_004)
+def foods():
+    fish_cracker_before = store_item["fish_crackers"]
+    store_item["fish_crackers"] -= 100
+    player_status["health"] += 100
+    print(f"""
+There is {store_item['fish_crackers']} out of {fish_cracker_before} fish crackers
+You've have {player_status['health']} health
+    """)
 
-convo_005(restult_004)
-convo_006()
-result_005 = shop()
-print(result_005)
 
+def store():
+    x = input(">>> ").lower()
+    if not x or x:
+        print("You've entered the store.")
+    x = input(">>> ").lower()
+    if not x or x:
+        print("Please select a choice.")
+        print("B for bullets C for fish crackers.")
+    x = input(">>> ").lower()
+    if x == "b":
+        bullets()
+    elif x == "c":
+        foods()
+
+
+# this is the execution area
+# convo_001()
+# convo_002()
+# convo_003()
+# restult_004 = convo_004()
+# convo_005(restult_004)
+# convo_006()
+store()
+convo_007()
 # NOTE: go in a linear pattern, choices matters
+
+"""
+WHAT I'VE LEARNED:
+
+# def fuction will reset the parameters when you call the arguement.
+# 
+"""
